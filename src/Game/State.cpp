bed8059b62f6c8763ec56277a7be157a71708390
triedal <triedal@madmobile.com>
2016-07-06 13:51:45 -0400

    spacing issues

--- src/Game/State.cpp
+++ src/Game/State.cpp
@@ -3,22 +3,22 @@
 
 State::State(int a, int b, int c)
 {
-  piles[0] = a;
-  piles[1] = b;
-  piles[2] = c;
-  this->sort();
+    piles[0] = a;
+    piles[1] = b;
+    piles[2] = c;
+    this->sort();
 }
 
 // sorts piles in desc order
 void State::sort()
 {
-	int tmp = 0;
-	for (int i=0; i < 3; ++i)
-		for (int j=i+1; j < 3; ++j)
-			if (piles[i] < piles[j])
-			{
-				tmp = piles[i];
-				piles[i] = piles[j];
-				piles[j] = tmp;
-			}
+    int tmp = 0;
+    for (int i=0; i < 3; ++i)
+       for (int j=i+1; j < 3; ++j)
+           if (piles[i] < piles[j])
+           {
+                tmp = piles[i];
+                piles[i] = piles[j];
+                piles[j] = tmp;
+            }
 }
