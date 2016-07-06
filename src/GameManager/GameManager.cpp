b1bbde2538c4dc3162ca4c758c72900576210cb3
triedal <triedal@madmobile.com>
2016-07-06 16:01:07 -0400

    implemented isWinner

--- src/GameManager/GameManager.cpp
+++ src/GameManager/GameManager.cpp
@@ -45,3 +45,40 @@ std::vector<std::vector<int> > GameManager::getMoves(State currState)
 
     return moves;
 }
+
+bool GameManager::isWinner(State currState)
+{
+    int p1 = currState.piles[0],
+        p2 = currState.piles[1],
+        p3 = currState.piles[2];
+
+    if (memo[p1][p2][p3] == UNSET)
+    {
+        std::vector<std::vector<int> > moves = getMoves(currState);
+        // One pile left
+        if ((p1 == 0 && p2 == 0) || (p1 == 0 && p3 == 0) || (p2 == 0 && p3 == 0))
+            memo[p1][p2][p3] = LOSING;
+        // Two piles left
+        else if (p3 == 0)
+        {
+            std::vector<int> move1Minus2 = moves[0];
+            State tmpState = State(move1Minus2[0], move1Minus2[1], move1Minus2[2]);
+            memo[p1][p2][p3] = isWinner(tmpState) ? LOSING : WINNING;
+        } 
+        // Three piles left
+        else 
+        {
+            std::vector<int> move1Minus2 = moves[0],
+                             move1Minus3 = moves[1],
+                             move2Minus3 = moves[2];
+
+            State s1 = State(move1Minus2[0], move1Minus2[1], move1Minus3[2]),
+                  s2 = State(move1Minus3[0], move1Minus3[1], move2Minus3[2]),
+                  s3 = State(move2Minus3[0], move2Minus3[1], move2Minus3[2]);
+
+            memo[p1][p2][p3] = (isWinner(s1) && isWinner(s2) && isWinner(s3)) ? LOSING : WINNING;
+        }
+        return memo[p1][p2][p3] == WINNING ? true : false;
+    }
+    return memo[p1][p2][p3] == WINNING ? true : false;
+}
